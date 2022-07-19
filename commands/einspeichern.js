52f03da69b51cbb030ece7f845ae4d2ff369ad97
ChuangSheep <44814054+ChuangSheep@users.noreply.github.com>
2022-07-19 14:47:02 +0200

    feat: add index.js feat: add deploy_commands.js feat: add einspeichern and klopfen commands

---
+++ commands/einspeichern.js
@@ -0,0 +1,67 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const axios = require("axios").default;
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('einspeichern')
+    .setDescription('Speichern die letzte Nachricht (eine|s Nutzers|in) als ihren/seinen Nasenspruch ein!')
+    .addUserOption(option => option.setName('target').setDescription('Wem gehört denn dieser Nasenspruch?')),
+  async execute(interaction, client) {
+    user = interaction.options.getUser("target");
+
+    channelId = interaction.channelId;
+    channel = await client.channels.fetch(channelId);
+
+    // find last sent message
+    msgs = await channel.messages.fetch({ limit: 6 });
+    let lastMessage = msgs.first();
+    if (user) {
+      for (const msg of msgs) {
+        if (msg[1].author.id === user.id) {
+          lastMessage = msg[1];
+          break;
+        }
+      }
+    }
+
+    // check if creterias are fulfilled
+    // timeout < 30s
+    if (Date.now() - lastMessage.createdTimestamp > 30000) {
+      await interaction.reply(`Die letzte Nachricht war leider zu lange her.`);
+      console.log(`[INFO] (einspeichern) Fail: MSG timed out.`);
+      return;
+    }
+
+
+    console.log(lastMessage);
+    if (!lastMessage.author.bot && lastMessage.content) {
+      // get displayname
+      const guild = await client.guilds.cache.get(lastMessage.guildId);
+      const gmember = await guild.members.fetch(lastMessage.author.id);
+      console.log(gmember);
+      const displayname = gmember.displayName;
+
+
+      // Database communication
+      let res = await axios.post(`${process.env.API_BASE_URL}`, {
+        userid: lastMessage.author.id,
+        susername: displayname,
+        scontent: lastMessage.content
+      }, {
+        headers: {
+          Authorization: `Bearer ${process.env.DATABASE_TOKEN}`,
+        }
+      }
+      )
+
+      // response msg
+      await interaction.reply(`"${lastMessage.content}" von "${displayname}" ist eingespeichert.`);
+      console.log(`[INFO] (einspeichern) Success: Saved '${lastMessage.content}' by ${lastMessage.author.id} (${displayname})`);
+    }
+    else {
+      // cannot find content
+      await interaction.reply(`Leider konnte kein gültiger Nasenspruch gefunden werden.`);
+      console.log(`[INFO] (einspeichern) Fail: No MSG fetched.`);
+    }
+  },
+};
---
+++ commands/klopfen.js
@@ -0,0 +1,45 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const axios = require("axios").default;
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('klopfen')
+    .setDescription('Der Nasenspruch von der Person vortragen!')
+    .addUserOption(option => option
+      .setName('target')
+      .setDescription('Wem gehört denn dieser Nasenspruch?')
+      .setRequired(true)),
+  async execute(interaction, client) {
+    user = interaction.options.getUser("target");
+    userid = user.id;
+
+    // Database communication
+    try {
+      let res = await axios.get(`${process.env.API_BASE_URL}?userid=${userid}`,{
+        headers: {
+          Authorization: `Bearer ${process.env.DATABASE_TOKEN}`,
+        }
+      }
+      );
+
+      console.log(res.data);
+      if (res) {
+        await interaction.reply(`"${res.data.scontent}" -- ${res.data.susername}`);
+        console.log(`[INFO] (klopfen) Success: Replied '${JSON.stringify(res.data)}'`);
+      }
+      else {
+        await interaction.reply(`Unbekannter Fehler.`);
+        console.log(`[INFO] (klopfen) Fail: Unknown failure.`);
+      }
+    } catch (e) {
+      console.log(e.response.status);
+      if (e.response.status === 404) {
+        await interaction.reply(`Für die gegebenen Person wurde noch kein Nasenspruch gespeichert.`);
+        console.log(`[INFO] (klopfen) Fail: No MSG fetched from API.`);
+      }
+      else {
+        throw e;
+      }
+    }
+  },
+};
